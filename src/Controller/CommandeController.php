6fe68774d58fdd8b091a883e9cdb5eb13176cd3b
NathanBarbier <81628062+NathanBarbier@users.noreply.github.com>
2022-12-09 14:27:46 +0100

    ajout page commande + page account + correction bug homepage (#7)

---
+++ src/Controller/CommandeController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Panier;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+#[Route('{_locale}/commande')]
+class CommandeController extends AbstractController
+{
+    protected EntityManagerInterface $em;
+    protected TranslatorInterface $translator;
+
+    public function __construct (
+        EntityManagerInterface $em,
+        TranslatorInterface $translator,
+    ) {
+        $this->em = $em;
+        $this->translator = $translator;
+    }
+    #[Route('/show/{id}', name: 'app_commande')]
+    public function index(Panier $panier = null): Response
+    {
+        if (null === $panier) {
+            $this->addFlash('warning', $this->translator->trans('commande.introuvable'));
+            return $this->redirectToRoute('app_home');
+        }
+
+
+        return $this->render('commande/index.html.twig', [
+            'panier' => $panier,
+        ]);
+    }
+}
