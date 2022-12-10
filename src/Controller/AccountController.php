a85fd41cd69f8b02212863285750fcbe697c6e00
Nathan Barbier <n.barbier001@gmail.com>
2022-12-10 10:51:58 +0100

    page Account

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,17 +2,41 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 #[Route('{_locale}/account')]
 class AccountController extends AbstractController
 {
-    #[Route('/', name: 'app_account')]
-    public function index(): Response
+    protected EntityManagerInterface $em;
+    protected TranslatorInterface $translator;
+
+    public function __construct (
+        EntityManagerInterface $em,
+        TranslatorInterface $translator,
+    ) {
+        $this->em = $em;
+        $this->translator = $translator;
+    }
+
+    #[Route('/show/{id}', name: 'app_account')]
+    public function index(User $user = null): Response
     {
+        /** redirection avec message si le user n'existe pas */
+        if (null === $user) {
+            $this->addFlash('warning', $this->translator->trans('user.introuvable'));
+            return $this->redirectToRoute('app_home');
+        }
+
+        $form = $this->createForm(UserType::class, $user);
+
         return $this->render('account/index.html.twig', [
+            'form' => $form,
             'utilisateur' => $this->getUser(),
             'paniers' => array_reverse($this->getUser()->getPaniers()->toArray())
         ]);
