8a763ffabd4babef208c090b9548936281227960
NathanBarbier <81628062+NathanBarbier@users.noreply.github.com>
2022-12-11 17:10:51 +0100

    Update src/Controller/ContenuPanierController.php

--- src/Controller/ContenuPanierController.php
+++ src/Controller/ContenuPanierController.php
@@ -8,7 +8,6 @@ use App\Entity\Produit;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
