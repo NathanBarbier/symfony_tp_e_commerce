6028e8797a37eb036c0c40935179a8e0278f6304
Florian Gastel <99323164+Otakk@users.noreply.github.com>
2022-12-11 17:08:28 +0100

    Flooo (#13)

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,41 @@
+// ouvrir la modal afin d'ajouter un produit
+function OpenAddModal(){
+    let add_modal = document.querySelector('#add_modal');
+    let closeByBg = document.querySelectorAll('.closeByBg')[0];
+
+    add_modal.classList.add('active');
+    closeByBg.style.display = 'block'
+}
+
+// fermer la modal ajouter un produit
+function closeModal() {
+    let add_modal = document.querySelector('#add_modal');
+    let closeByBg = document.querySelectorAll('.closeByBg')[0];
+    
+    closeByBg.style.display = 'none'
+    add_modal.classList.remove('active');
+}
+
+// ouvrir la modal panier
+function OpenBasketModal() {
+    let basket_modal = document.querySelector('#basket_modal');
+    let closeByBg = document.querySelectorAll('.closeByBg')[0];
+
+    basket_modal.classList.add('active');
+    closeByBg.style.display = 'block';
+}
+
+// fermer la modal panier
+function closeBasketBasket() {
+    let basket_modal = document.querySelector('#basket_modal');
+    let closeByBg = document.querySelectorAll('.closeByBg')[0];
+    
+    closeByBg.style.display = 'none'
+    basket_modal.classList.remove('active');
+}
+
+
+//  Compteur de la qte pour ajouter 1 au panier
 function addOne(){
     let count = document.querySelector('#count').value
     let span_qte = document.querySelector('#span_qte').innerHTML
@@ -8,12 +46,12 @@ function addOne(){
     }
 }
 
+//  Compteur de la qte pour retirer 1 au panier
 function removeOne() {
     let count = document.querySelector('#count').value
 
     if(count > 1){
-    count = --count
-    document.querySelector('#count').value = count
-    console.log(count);
+        count = --count
+        document.querySelector('#count').value = count
     }
 }
